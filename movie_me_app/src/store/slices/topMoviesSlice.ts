f1daf70fdd2b5c4ac80d9f90b55f1d666bcfdcbc
r00nie <banasiakkacperr@gmail.com>
2023-04-06 14:29:18 +0200

    Creating app store. Implementing two slices of app store.

---
+++ movie_me_app/src/store/slices/topMoviesSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const fetchTopMovies = createAsyncThunk(
+  "topMovies/fetchTopMovies",
+  async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "2468e0513amsheb6ff72fcd44999p151290jsnafd75bae0c41",
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      },
+    };
+
+    const response = await fetch(
+      "https://moviesdatabase.p.rapidapi.com/titles?list=top_rated_250&limit=10",
+      options
+    ).catch((err) => console.error(err));
+
+    if (!response) {
+      throw new Error("Response is not valid");
+    }
+
+    const data = await response.json();
+    return data.results;
+  }
+);
+
+const topMoviesSlice = createSlice({
+  name: "topMovies",
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTopMovies.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(fetchTopMovies.rejected, (state, action) => {
+        console.log("Error while fetching data...");
+        return state;
+      });
+  },
+});
+
+export const {} = topMoviesSlice.actions;
+export { fetchTopMovies };
+export default topMoviesSlice.reducer;
---
+++ movie_me_app/src/store/slices/userMoviesSlice.ts
@@ -0,0 +1,98 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movies = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    releaseDate: "1994-09-23",
+    description:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    photoUrl: "https://www.example.com/shawshank-redemption.jpg",
+    rating: 9.3,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    releaseDate: "1972-03-24",
+    description:
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    photoUrl: "https://www.example.com/godfather.jpg",
+    rating: 9.2,
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    releaseDate: "2008-07-18",
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    photoUrl: "https://www.example.com/dark-knight.jpg",
+    rating: 9.0,
+  },
+  {
+    id: 4,
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    director: "Peter Jackson",
+    releaseDate: "2001-12-19",
+    description:
+      "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+    photoUrl: "https://www.example.com/lotr-fellowship.jpg",
+    rating: 8.8,
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    director: "Robert Zemeckis",
+    releaseDate: "1994-07-06",
+    description:
+      "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.",
+    photoUrl: "https://www.example.com/forrest-gump.jpg",
+    rating: 8.8,
+  },
+  {
+    id: 6,
+    title: "Inception",
+    director: "Christopher Nolan",
+    releaseDate: "2010-07-16",
+    description:
+      "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    photoUrl: "https://www.example.com/inception.jpg",
+    rating: 8.7,
+  },
+  {
+    id: 7,
+    title: "The Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+    releaseDate: "1999-03-31",
+    description:
+      "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    photoUrl: "https://www.example.com/matrix.jpg",
+    rating: 8.7,
+  },
+];
+
+const userMoviesSlice = createSlice({
+  name: "userMovies",
+  initialState: movies,
+  reducers: {
+    addMovie: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteMovie: (state, action) => {
+      return state.filter((movie) => movie.id !== action.payload);
+    },
+    editMovie: (state, action) => {
+      const movieIndex = state.findIndex(
+        (movie) => movie.id === action.payload.id
+      );
+      if (movieIndex !== -1) {
+        state[movieIndex] = action.payload;
+      }
+    },
+  },
+});
+
+export const { addMovie, deleteMovie, editMovie } = userMoviesSlice.actions;
+export default userMoviesSlice.reducer;
